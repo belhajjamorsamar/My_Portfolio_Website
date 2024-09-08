28a2e2a18af75f11af88053248abbf8048e925d3
Samar bel hajj amor <belhajjamorsamar@gmail.com>
2024-09-08 16:12:24 +0200

    add a transition animation for the navbar

--- my-app/components/Stairs.jsx
+++ my-app/components/Stairs.jsx
@@ -13,18 +13,39 @@ const stairAnimation ={
     },
 };
 
+//calculate the reserve index fot staggred delay
+const reverseIndex = (index)=>{
+    const totalSteps =6 ; //
+    return totalSteps -index - 1 ;
+};
 
 const Stairs = () => {
-  return 
+  return (
     <>
-      
-
-{/*render 6 motion divs , each representing a step of the Stairs .*
-each div  will have the same animation defined by the Stair animation object .
- the delay for each div is  calculated sinamically based an it's reserved index, 
- creating a staggered  effect with decreasing delay for each subsequent step. */}
-    </>
-  
+{/*render 6 motion divs , each representing a step of the Stairs.
+   each div  will have the same animation defined by the Stair animation object .
+   the delay for each div is  calculated sinamically based an it's reserved index, 
+   creating a staggered  effect with decreasing delay for each subsequent step. 
+*/}
+ 
+ {[...Array(6)].map((_ ,index)=>{
+    return (
+    <motion.div 
+    key={index} 
+    variants={stairAnimation}
+    initial ="initial"
+    animate="animate" 
+    exit="exit" 
+    transition={{
+        duration:0.4,
+        ease:'easeInOut',
+        delay: reverseIndex(index)*0.1,
+    }}
+     className="h-full w-full bg-white relative "/>
+    );
+ })}  
+ </>
+  ); 
 }
 
 export default Stairs
